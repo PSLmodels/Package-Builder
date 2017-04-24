3c673bb63294d610bb54f564652cf7ed97d75124
Peter Steinberg <psteinberg@continuum.io>
2017-04-24 14:59:34 -0700

    fix the version string being uploaded if file exists

--- build_release.sh
+++ build_release.sh
@@ -84,10 +84,13 @@ anaconda_upload(){
     cd $PKGS_TO_UPLOAD || return 1;
     export ret=0;
     export version=$2;
+    export pkg=$3;
     if [ "$OSPC_ANACONDA_CHANNEL" = "" ];then
         export OSPC_ANACONDA_CHANNEL=dev;
     fi
-    if [ "$OSPC_ANACONDA_CHANNEL" = "dev" ];then
+    export file_exists=0;
+    anaconda search -t conda $pkg | grep ospc | grep $version && export file_exists=1;
+    if [ "$file_exists" = "1" ];then
         export version="${version}-dev";
     fi
     if [ "$SKIP_ANACONDA_UPLOAD" = "" ];then
@@ -108,13 +111,21 @@ anaconda_upload(){
 convert_packages(){
     export build_file=$1;
     export version=$2;
+    export pkg=$3;
+    if [ "$pkg" = "Tax-Calculator" ];then
+        export pkg=taxcalc;
+    elif [ "$pkg" = "OG-USA" ];then
+        export pkg=btax
+    else
+        export pkg=ogusa
+    fi
     cd $PKGS_TO_UPLOAD || return 1;
     msg Convert $build_file for platforms;
     msg conda convert -p all $build_file -o .
 
     conda convert -p all $build_file -o . || return 1;
     for platform in win-32 win-64 linux-64 linux-32 osx-64; do
-        ls $platform && anaconda_upload ./${platform}/*-${version}-*.tar.bz2 "${version}";
+        ls $platform && anaconda_upload ./${platform}/*-${version}-*.tar.bz2 "${version}" $pkg;
     done
     anaconda_upload $build_file || return 1;
     return 0;
@@ -122,7 +133,7 @@ convert_packages(){
 replace_version(){
     replacement=$1;
     grepper=$2;
-    old_ifs=$IFS;
+    old_ifs="$IFS";
     IFS='' ; while read line ;do echo "$line" | grep $grepper && echo "$replacement" >> meta2.yaml  || echo "$line" >> meta2.yaml  ; done < meta.yaml
     IFS=$old_ifs;
     mv meta2.yaml meta.yaml;
@@ -159,7 +170,7 @@ build_one_pkg(){
     msg RUN: conda build --use-local --python $python_version ${USE_PYTHON_RECIPE};
     conda build -c ospc --python $python_version ${USE_PYTHON_RECIPE} || return 1;
     msg RUN: conda convert packages for python $python_version;
-    convert_packages "$(conda build --python $python_version ${USE_PYTHON_RECIPE} --output)" ${2} || return 1;
+    convert_packages "$(conda build --python $python_version ${USE_PYTHON_RECIPE} --output)" ${2} $1 || return 1;
     return 0;
 }
 
