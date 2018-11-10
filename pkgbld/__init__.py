200e67d58e3e39d1edbf49fa5f4d504c8a352187
Martin Holmer <martin.holmer@gmail.com>
2018-11-10 15:05:55 -0500

    Add setup and versioneer files

--- pkgbld/__init__.py
+++ pkgbld/__init__.py
@@ -2,3 +2,7 @@
 Specify what is available to import from the pkgbld package.
 """
 from pkgbld.release import release
+
+from pkgbld._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
