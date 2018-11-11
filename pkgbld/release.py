30c6818b0c1e491767928b46a71b65c799d7d075
Martin Holmer <martin.holmer@gmail.com>
2018-11-11 11:51:27 -0500

    Add extra utility function and more release logic

--- pkgbld/release.py
+++ pkgbld/release.py
@@ -5,15 +5,27 @@ Policy Simulaton Library (PSL) model Anaconda-Cloud package release logic.
 # pycodestyle release.py
 # pylint --disable=locally-disabled release.py
 
+import os
 import re
+import shutil
+import pkgbld.utils as u
 
 
 PYTHON_VERSIONS = ['3.6']
 OS_PLATFORMS = ['osx-64', 'linux-64', 'win-32', 'win-64']
 
-GITHUB_URL = 'https:/github.com/open-source-economics/'
-CONDA_CHANNELS = ['pslmodels']
+GITHUB_URL = 'https://github.com/open-source-economics'
 ANACONDA_USER = 'pslmodels'
+ANACONDA_CHANNEL = ANACONDA_USER
+HOME_DIR = os.path.expanduser('~')
+ANACONDA_TOKEN_FILE = os.path.join(
+    HOME_DIR,
+    '.{}_anaconda_token'.format(ANACONDA_USER)
+)
+WORKING_DIR = os.path.join(
+    HOME_DIR,
+    'temporary_pkgbld_working_dir'
+)
 
 
 def release(repo_name, pkg_name, version):
@@ -25,18 +37,20 @@ def release(repo_name, pkg_name, version):
     Parameters
     ----------
     repo_name: string
-        repository name appended to GITHUB_URL
+        model repository name appended to GITHUB_URL
 
     pkg_name: string
-        package name for repository specified by repo_name
+        model package name for repository specified by repo_name
 
     version: string
-        version string consistent with semantic versioning
+        model version string consistent with semantic versioning;
+        must be a release tag in the model repository
 
     Raises
     ------
     ValueError:
         if parameters are not valid
+        if ANACONDA_TOKEN_FILE does not exist
 
     Returns
     -------
@@ -58,4 +72,47 @@ def release(repo_name, pkg_name, version):
         msg = 'version=<{}> does not follow semantic-versioning rules'
         raise ValueError(msg.format(version))
 
-    print('Hello from Package-Builder release function')
+    # get token
+    if not os.path.isfile(ANACONDA_TOKEN_FILE):
+        msg = 'Anaconda token file {} does not exist'
+        raise ValueError(msg.format(ANACONDA_TOKEN_FILE))
+    with open(ANACONDA_TOKEN_FILE, 'r') as tfile:
+        token = tfile.read()
+
+    # make empty working directory
+    if os.path.isdir(WORKING_DIR):
+        shutil.rmtree(WORKING_DIR)
+    os.mkdir(WORKING_DIR)
+    os.chdir(WORKING_DIR)
+
+    print('Package-Builder release:')
+    print('repo_name = {}'.format(repo_name))
+    print('pkg_name = {}'.format(pkg_name))
+    print('version = {}'.format(version))
+    print('token = {}'.format(token))
+
+    # clone model repository and checkout model version
+    cmd = 'git clone {}/{}/'.format(GITHUB_URL, repo_name)
+    u.os_call(cmd)
+    os.chdir(repo_name)
+    cmd = 'git checkout -b v{ver} {ver}'.format(ver=version)
+    u.os_call(cmd)
+
+    # specify version in repository's conda.recipe/meta.yaml file
+    u.specify_version(version)
+
+    # build and upload model package for each Python version and OS platform
+    local_platform = u.conda_platform_name()
+    for pyver in PYTHON_VERSIONS:
+        pyver_list = pyver.split('.')
+        pystr = pyver_list[0] + pyver_list[1]
+        cmd = ('conda build --python {} --old-build-string '
+               '--channel {} --override-channels '
+               '--no-anaconda-upload --output-folder builds '
+               'conda.recipe').format(pyver, ANACONDA_CHANNEL)
+        u.os_call(cmd)
+        # TODO: more code here
+
+    # remove working directory and its contents
+    os.chdir(HOME_DIR)
+    shutil.rmtree(WORKING_DIR)
--- pkgbld/tests/test_utils.py
+++ pkgbld/tests/test_utils.py
@@ -6,23 +6,23 @@ Tests of Package-Builder utility functions.
 # pylint --disable=locally-disabled test_utils.py
 
 import pytest
-from pkgbld.utils import call, call_output
+from pkgbld.utils import os_call, os_call_output
 
 
-def test_call():
+def test_os_call():
     """
     Test call utility function.
     """
-    call('hostname')
+    os_call('hostname')
     with pytest.raises(OSError):
-        call('illegal_os_command')
+        os_call('illegal_os_command')
 
 
-def test_call_output():
+def test_os_call_output():
     """
     Test call_output utility function.
     """
-    out = call_output('hostname')
+    out = os_call_output('hostname')
     assert out
     with pytest.raises(OSError):
-        out = call_output('illegal_os_command')
+        out = os_call_output('illegal_os_command')
