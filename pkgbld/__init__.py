6ccb0f7bbae2e14380a137349cebb92b74b64395
Martin Holmer <martin.holmer@gmail.com>
2018-11-12 21:14:09 -0500

    Remove versioneer.py and _version.py files

--- pkgbld/__init__.py
+++ pkgbld/__init__.py
@@ -5,6 +5,4 @@ from pkgbld.utils import *
 from pkgbld.release import *
 from pkgbld.cli import *
 
-from pkgbld._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+__version__ = '0.0.0'
--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-import versioneer
-versioneer.VCS = 'git'
-versioneer.versionfile_source = 'pkgbld/_version.py'
-versioneer.versionfile_build = 'pkgbld/_version.py'
-versioneer.tag_prefix = ''  # tags are like 1.2.0
-versioneer.parentdir_prefix = 'pkgbld-'
-# above dirname like 'pkgbld-1.2.0'
-
 try:
     from setuptools import setup
 except ImportError:
@@ -14,8 +6,7 @@ except ImportError:
 with open('README.md') as f:
         longdesc = f.read()
 
-version = versioneer.get_version()
-cmdclass = versioneer.get_cmdclass()
+version = '0.0.0'
 
 config = {
     'description': 'Package Builder',
@@ -24,7 +15,6 @@ config = {
     'description': 'pkgbld',
     'long_description': longdesc,
     'version': version,
-    'cmdclass': cmdclass,
     'license': 'CC0 1.0 Universal public domain dedication',
     'packages': ['pkgbld'],
     'include_package_data': True,
